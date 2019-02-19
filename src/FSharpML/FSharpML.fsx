99e3daaa9e18d0fac5b85978eaf1cede176498b3
ZimmerD <davidzimmer91@gmail.com>
2019-02-19 17:30:50 +0100

    add paket microsoft.ml add FSharpML.fsx add SpamDetectionConsoleApp code and data

---
+++ src/FSharpML/FSharpML.fsx
@@ -0,0 +1,20 @@
+
+#r "netstandard.dll"
+#I @"C:\Users\david\Source\Repos\netCoreRepos\FSharpML\packages" 
+
+#r @"System.Memory\lib\netstandard2.0\System.Memory.dll"
+//#r @"System.Collections.Immutable\lib\netstandard2.0\System.Collections.Immutable.dll"
+#r @"Microsoft.Data.DataView\lib\netstandard2.0\Microsoft.Data.DataView.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.Transforms.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.FastTree.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.Data.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.KMeansClustering.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.StandardLearners.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.Sweeper.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.PCA.dll"
+#r @"Microsoft.ML/lib\netstandard2.0\Microsoft.ML.Core.dll"
+
+
+#r @"Microsoft.ML.CpuMath\lib\netstandard2.0\Microsoft.ML.CpuMath.dll"
+#r @"system.runtime.compilerservices.unsafe\lib\netstandard2.0\System.Runtime.CompilerServices.Unsafe.dll"
+//#I @"Microsoft.ML\runtimes\win-x64\native\"
