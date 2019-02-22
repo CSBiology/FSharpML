1671cfa87d2dcd96fdf8e7c01cfd05b5b66b4fab
ZimmerD <davidzimmer91@gmail.com>
2019-02-22 17:23:00 +0100

    extended Introduction

--- docsrc/content/Introduction.fsx
+++ docsrc/content/Introduction.fsx
@@ -10,13 +10,6 @@ Sample Spam detection
 
 **)
 
-#load "../../bin/FSharpML/netstandard2.0/FSharpML.fsx"
-//#load "../../FSharpML.fsx"
-
-open System;
-open Microsoft.ML
-open Microsoft.ML.Data;
-open FSharpML
 
 (**
 Introduction
@@ -27,12 +20,16 @@ manner, while maintaining the functional style of F#.
 
 After installing the package via Nuget we can load the delivered reference script and start using ML.Net in conjunction with FSharpML.
 *)
-#load "../../bin/FSharpML/netstandard2.0/FSharpML.fsx"
 
+#load "../../bin/FSharpML/netstandard2.0/FSharpML.fsx"
+#r @"../../lib/Formatting/FSharp.Plotly.dll"
+open FSharp.Plotly
 open System;
 open Microsoft.ML
 open Microsoft.ML.Data;
 open FSharpML
+open Microsoft.ML.Core.Data
+
 
 (**
 To get a feel how this library handles ML.Net operations we rebuild the Spam Detection tutorial (https://github.com/dotnet/machinelearning-samples/tree/master/samples/fsharp/getting-started/BinaryClassification_SpamDetection)
@@ -64,7 +61,7 @@ At this point we will see FSharpML comming into play enabling us to use the belo
 We will now create an EstimatorChain which converts the text label to a bool then featurizes the text, and add a linear trainer.
 *)
 
-let estimatorModel = 
+let estimatorChain = 
     EstimatorChain()
     |> Estimator.append (mlContext.Transforms.Conversion.ValueMap(["ham"; "spam"],[false; true],[| struct ("Label", "LabelText") |]))
     |> Estimator.append (mlContext.Transforms.Text.FeaturizeText("Features", "Message"))
@@ -77,7 +74,7 @@ explicitly. For this we created the type EstimatorModel which contains our Estim
 lambda expression were we can define which method we want of our context.
 *)
 
-let estimatorModel' = 
+let estimatorModel = 
     EstimatorModel.create mlContext
     |> EstimatorModel.appendBy (fun mlc -> mlc.Transforms.Conversion.ValueMap(["ham"; "spam"],[false; true],[| struct ("Label", "LabelText") |]))
     |> EstimatorModel.appendBy (fun mlc -> mlc.Transforms.Text.FeaturizeText("Features", "Message"))
@@ -86,14 +83,85 @@ let estimatorModel' =
 
 (**
 Way better. Now we can concentrate on machine learning. So lets start by fitting our EstimatorModel to the complete data set.
+The return value of this process is a so called TransformerModel, which contains a trained EstimatorChain
+and can be used to transform unseen data. For this we want to split the data we previously put in scope into 
+two fractions. One to train the model and a remainder to evaluate the model. 
 *)
 
-let model = EstimatorModel.fit data estimatorModel'
-let out   = TransformerModel.transform data model
+let trainTestSplit = 
+    data
+    |> Data.BinaryClassification.initTrainTestSplit(estimatorModel.Context,Testfraction=0.1) 
+
+let trainedModel = 
+    estimatorModel
+    |> EstimatorModel.fit trainTestSplit.TrainingData
+
+let evaluationMetrics = 
+    trainedModel
+    |> Evaluation.BinaryClassification.InitEvaluate() trainTestSplit.TestData
+
+evaluationMetrics.Accuracy
 
 (**
-Evaluation, see how good we are, may missleading because we evaluated on the training data set
+Now that we can examine the metrics of our model evaluation, see that we have a accuracy of 0.99 
+and be tempted to use it in production so lets test it first with some examples.
 *)
+[<CLIMutable>]
+type SpamInput = 
+    {
+        LabelText : string
+        Message : string
+    }
+
+let results = 
+    [
+        "That's a great idea. It should work."
+        "free medicine winner! congratulations"
+        "Yes we should meet over the weekend!"
+        "you win pills and free entry vouchers"
+    ] 
+    |> List.map (fun message ->{LabelText = "unknown"; Message = message})
+    |> (Prediction.BinaryClassification.predict trainedModel)
+    |> Array.ofSeq
+
+(**
+As we see, even so our accuracy when evaluating the model on the test data set was very high, it does not set the correct lable true, to the
+second and the fourth message which look a lot like spam. Lets examine our training data set:
+*)
+let labels,counts =
+    mlContext.CreateEnumerable<SpamInput>(data,false)
+    |> Seq.countBy (fun (message) -> 
+                    message.LabelText
+                  )
+    |> Seq.unzip
+
+Chart.Doughnut(counts,labels)
+|> Chart.Show
+
+(**
+The chart clearly shows that the data we learned uppon is highly inhomogenous. We have a lot more ham than spam, which is generally preferable but 
+but our models labeling threshold is clearly to high. Lets have a look at the precision recall curves of our model.
+*)
+
+//(trainedModel.TransformerChain.LastTransformer |> Transformer.downcastTransformer).CreatePredictionEngine<SpamInput,SpamPrediction>(mlContext)
+let newModel = 
+    let lastTransformer = 
+        BinaryPredictionTransformer<IPredictorProducing<float32>>(
+            mlContext, 
+            trainedModel.TransformerChain.LastTransformer.Model, 
+            trainedModel.TransformerChain.GetOutputSchema(data.Schema), 
+            trainedModel.TransformerChain.LastTransformer.FeatureColumn, 
+            threshold = 0.15f, 
+            thresholdColumn = DefaultColumnNames.Probability);
+    let parts = trainedModel.TransformerChain |> Seq.toArray
+    parts.[parts.Length - 1] <- lastTransformer :> _
+    TransformerChain<ITransformer>(parts)
+
+
+// Create a PredictionFunction from our model 
+let predictor = newModel.CreatePredictionEngine<SpamInput, SpamPrediction>(mlContext);
+
+
 
 (**
 TrainTestSplit, to get a more meaningful evaluation
@@ -156,9 +224,23 @@ classify examples
 //    parts.[parts.Length - 1] <- lastTransformer :> _
 //    TransformerChain<ITransformer>(parts)
 
+[<CLIMutable>]
+type SpamInput = 
+    {
+        LabelText : string
+        Message : string
+    }
 
-//// Create a PredictionFunction from our model 
-//let predictor = newModel.CreatePredictionEngine<SpamInput, SpamPrediction>(mlContext);
+[<CLIMutable>]
+type SpamPrediction = 
+    {
+        PredictedLabel : bool
+        Score : float32
+        Probability : float32
+    }
+
+// Create a PredictionFunction from our model 
+let predictor = trainedModel.TransformerChain.CreatePredictionEngine<SpamInput, SpamPrediction>(estimatorModel.Context);
 
 
 //let classify (p : PredictionEngine<_,_>) x = 
@@ -174,4 +256,20 @@ classify examples
 //] 
 //|> List.iter (classify predictor)
 
+
+
+//[<CLIMutable>]
+//type SpamInput = 
+//    {
+//        LabelText : string
+//        Message : string
+//    }
+
+//[<CLIMutable>]
+//type SpamPrediction = 
+//    {
+//        PredictedLabel : bool
+//        Score : float32
+//        Probability : float32
+//    }
 
--- docsrc/content/SpamDetection.fsx
+++ docsrc/content/SpamDetection.fsx
@@ -11,102 +11,99 @@ Sample Spam detection
 **)
 
 #load "../../bin/FSharpML/netstandard2.0/FSharpML.fsx"
-//#load "../../FSharpML.fsx"
 
 open System;
 open Microsoft.ML
 open Microsoft.ML.Data;
 open FSharpML
-open FSharpML.EstimatorModel
+
+open Microsoft.ML
+open Microsoft.ML.Data
+open Microsoft.ML.Core.Data
+open Microsoft.Data.DataView
+[<CLIMutable>]
+type SpamInput = 
+    {
+        LabelText : string
+        Message : string
+    }
+
+[<CLIMutable>]
+type SpamPrediction = 
+    {
+        PredictedLabel : bool
+        Score : float32
+        Probability : float32
+    }
 
 
+let classify (p : PredictionEngine<_,_>) x = 
+    let prediction = p.Predict({LabelText = ""; Message = x})
+    printfn "The message '%s' is %b" x prediction.PredictedLabel
 
-
-let trainDataPath  = (__SOURCE_DIRECTORY__  + "./data/SMSSpamCollection.txt")
 
 let mlContext = MLContext(seed = Nullable 1)
 
-let reader = 
-    mlContext.Data.CreateTextLoader(
+let trainDataPath  = (__SOURCE_DIRECTORY__  + "./data/SMSSpamCollection.txt")
+
+
+let data = 
+    mlContext.Data.ReadFromTextFile(trainDataPath,
         columns = 
             [|
                 TextLoader.Column("LabelText" , Nullable DataKind.Text, 0)
                 TextLoader.Column("Message" , Nullable DataKind.Text, 1)
             |],
-            hasHeader = false,
-            separatorChar = '\t')
+        hasHeader = false,
+        separatorChar = '\t')
     
-let data = reader.Read(trainDataPath)
-
 // Create the estimator which converts the text label to a bool then featurizes the text, and add a linear trainer.
-let estimatorModel = 
-    EstimatorModel.create mlContext
-    |> EstimatorModel.appendBy (conversionValueMap ["ham"; "spam"] [false; true] [| struct ("Label", "LabelText") |])
-    |> EstimatorModel.appendBy (fun mlc -> mlc.Transforms.Text.FeaturizeText("Features", "Message"))
-    |> EstimatorModel.appendCacheCheckpoint
-    |> EstimatorModel.appendBy (fun mlc -> mlc.BinaryClassification.Trainers.LogisticRegression("Label", "Features"))
-
-
-
-
+let estimator = 
+    EstimatorChain()
+        .Append(mlContext.Transforms.Conversion.ValueMap(["ham"; "spam"], [false; true],[| struct ("Label", "LabelText") |]))
+        .Append(mlContext.Transforms.Text.FeaturizeText("Features", "Message"))
+        .AppendCacheCheckpoint(mlContext)
+        .Append(mlContext.BinaryClassification.Trainers.StochasticDualCoordinateAscent("Label", "Features"))
+        
 // Evaluate the model using cross-validation.
 // Cross-validation splits our dataset into 'folds', trains a model on some folds and 
 // evaluates it on the remaining fold. We are using 5 folds so we get back 5 sets of scores.
 // Let's compute the average AUC, which should be between 0.5 and 1 (higher is better).
-let cvResults = mlContext.BinaryClassification.CrossValidate(data, estimatorModel.EstimatorChain |> Estimator.downcastEstimator, numFolds = 5,stratificationColumn = null)
-let avgAuc = cvResults |> Seq.map (fun struct (metrics,_,_) -> metrics.Auc) |> Seq.average
+//let cvResults = mlContext.BinaryClassification.CrossValidate(data, Estimator.downcastEstimator estimator, numFolds = 5);
+//let avgAuc = cvResults |> Seq.map (fun struct (metrics,_,_) -> metrics.Auc) |> Seq.average
 //printfn "The AUC is %f" avgAuc
-
-
+    
 // Now let's train a model on the full dataset to help us get better results
-let model = EstimatorModel.fit data estimatorModel
-let out = TransformerModel.transform data model
-
-
-model
-|> Evaluation.BinaryClassificationEvalute() data
-
-/// run till here, then continue, breaks 
-let x = out.Preview().ColumnView
-/// run till here, works
-//x |> Seq.map (fun x -> x.Column.Name)
-
-//let downcastOtherPipeline (pipeline : ITransformer) =
-//    match pipeline with
-//    | :? IPredictionTransformer<IPredictor> as p -> p
-//    | _ -> failwith "The pipeline has to be an instance of IEstimator<ITransformer>."
-
-//let permutationMetrics = 
-//    mlContext.BinaryClassification.PermutationFeatureImportance(model.LastTransformer |> downcastOtherPipeline,out,"Label","Features",false,permutationCount=1)
+let model = estimator.Fit(data)
 
 // The dataset we have is skewed, as there are many more non-spam messages than spam messages.
 // While our model is relatively good at detecting the difference, this skewness leads it to always
 // say the message is not spam. We deal with this by lowering the threshold of the predictor. In reality,
 // it is useful to look at the precision-recall curve to identify the best possible threshold.
-//let newModel = 
-//    let lastTransformer = 
-//        BinaryPredictionTransformer<IPredictorProducing<float32>>(
-//            mlContext, 
-//            model.LastTransformer.Model, 
-//            model.GetOutputSchema(data.Schema), 
-//            model.LastTransformer.FeatureColumn, 
-//            threshold = 0.15f, 
-//            thresholdColumn = DefaultColumnNames.Probability);
-//    let parts = model |> Seq.toArray
-//    parts.[parts.Length - 1] <- lastTransformer :> _
-//    TransformerChain<ITransformer>(parts)
+let newModel = 
+    let lastTransformer = 
+        BinaryPredictionTransformer<IPredictorProducing<float32>>(
+            mlContext, 
+            model.LastTransformer.Model, 
+            model.GetOutputSchema(data.Schema), 
+            model.LastTransformer.FeatureColumn, 
+            threshold = 0.15f, 
+            thresholdColumn = DefaultColumnNames.Probability);
+    let parts = model |> Seq.toArray
+    parts.[parts.Length - 1] <- lastTransformer :> _
+    TransformerChain<ITransformer>(parts)
 
 
-//// Create a PredictionFunction from our model 
-//let predictor = newModel.CreatePredictionEngine<SpamInput, SpamPrediction>(mlContext);
+// Create a PredictionFunction from our model 
+let predictor = model.CreatePredictionEngine<SpamInput, SpamPrediction>(mlContext);
 
-//// Test a few examples
-//[
-//    "That's a great idea. It should work."
-//    "free medicine winner! congratulations"
-//    "Yes we should meet over the weekend!"
-//    "you win pills and free entry vouchers"
-//] 
-//|> List.iter (classify predictor)
+// Test a few examples
+[
+    "That's a great idea. It should work."
+    "free medicine winner! congratulations"
+    "Yes we should meet over the weekend!"
+    "you win pills and free entry vouchers"
+] 
+|> List.iter (classify predictor)
 
 
