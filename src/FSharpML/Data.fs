28c642aae28a7570ab3a601fcb224d02c6ef6ef5
ZimmerD <davidzimmer91@gmail.com>
2019-02-22 17:19:13 +0100

    refactored trainTestSplit into Data.fs.

--- src/FSharpML/Data.fs
+++ src/FSharpML/Data.fs
@@ -1,14 +1,109 @@
 namespace FSharpML
 
+open System
 open Microsoft.ML
 open Microsoft.ML.Core.Data
+open Microsoft.Data.DataView
+open Microsoft.ML.Data
+open System.Runtime.InteropServices
+
 
 
 module Data =
-    let a = 42
-    //type Reader =
-    //     static member intiLoadText (a:string) (b:int,path,?c:int) =
+    
+    type TrainTestSplit = {
+        TrainingData        : IDataView
+        TestData            : IDataView
+        SplitFraction       : float
+        StratificationColumn: string option
+        }
+ 
+    type BinaryClassification =
         
+        static member initTrainTestSplit
+            (
+                context:MLContext, 
+                ?Testfraction:float,
+                ?Stratification : string,
+                ?Seed : uint32 
+            ) =
+                let testFraction    = defaultArg Testfraction 0.
+                let stratification  = defaultArg Stratification null
+                let seed            = Option.toNullable Seed
+                
+                fun (data:IDataView)  ->                                                           
+                    let train,test = (context.BinaryClassification.TrainTestSplit(data,testFraction,stratification,seed)).ToTuple() 
+                    {TrainingData = train; TestData = test; SplitFraction = testFraction; StratificationColumn= Stratification}
 
-    //    static member loadText (a:string) (b:int,path,?c:int) =
-    //       a,b
+    type MulticlassClassification =
+
+        static member initTrainTestSplit
+            (
+                context:MLContext, 
+                ?Testfraction:float,
+                ?Stratification : string,
+                ?Seed : uint32 
+            ) =
+                let testFraction    = defaultArg Testfraction 0.
+                let stratification  = defaultArg Stratification null
+                let seed            = Option.toNullable Seed
+                
+                fun (data:IDataView)  ->                                                           
+                    let train,test = (context.MulticlassClassification.TrainTestSplit(data,testFraction,stratification,seed)).ToTuple() 
+                    {TrainingData = train; TestData = test; SplitFraction = testFraction; StratificationColumn= Stratification}
+                    
+    type Regression =
+        
+        static member initTrainTestSplit
+            (
+                context:MLContext, 
+                ?Testfraction:float,
+                ?Stratification : string,
+                ?Seed : uint32 
+            ) =
+                let testFraction    = defaultArg Testfraction 0.
+                let stratification  = defaultArg Stratification null
+                let seed            = Option.toNullable Seed
+                
+                fun (data:IDataView)  ->                                                           
+                    let train,test = (context.Regression.TrainTestSplit(data,testFraction,stratification,seed)).ToTuple() 
+                    {TrainingData = train; TestData = test; SplitFraction = testFraction; StratificationColumn= Stratification}
+                    
+    type Clustering =
+ 
+        static member initTrainTestSplit
+            (
+                context:MLContext, 
+                ?Testfraction:float,
+                ?Stratification : string,
+                ?Seed : uint32 
+            ) =
+                let testFraction    = defaultArg Testfraction 0.
+                let stratification  = defaultArg Stratification null
+                let seed            = Option.toNullable Seed
+                
+                fun (data:IDataView)  ->                                                           
+                    let train,test = (context.Clustering.TrainTestSplit(data,testFraction,stratification,seed)).ToTuple() 
+                    {TrainingData = train; TestData = test; SplitFraction = testFraction; StratificationColumn= Stratification}
+                    
+                       
+    type Ranking =
+  
+        static member initTrainTestSplit
+            (
+                context:MLContext, 
+                ?Testfraction:float,
+                ?Stratification : string,
+                ?Seed : uint32 
+            ) =
+                let testFraction    = defaultArg Testfraction 0.
+                let stratification  = defaultArg Stratification null
+                let seed            = Option.toNullable Seed
+                
+                fun (data:IDataView)  ->                                                           
+                    let train,test = (context.Ranking.TrainTestSplit(data,testFraction,stratification,seed)).ToTuple() 
+                    {TrainingData = train; TestData = test; SplitFraction = testFraction; StratificationColumn= Stratification}
+          
+
+        
+        //Not implemented in ML.net: AnomalyDetection
